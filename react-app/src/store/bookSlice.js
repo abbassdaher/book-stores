65bf1e1a8d137f43dea86997404960f62b7b025c
abbassDaher <abbassDaher1@outlook.com>
2024-04-22 10:43:09 +0300

    lifeCycle Action asyncthunk

--- react-app/src/store/bookSlice.js
+++ react-app/src/store/bookSlice.js
@@ -4,16 +4,30 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const getBooks = createAsyncThunk(
     'book/getBooks',
     async (_, thunkAPI) => {
+        // part 2
+        // ----------------------------------------------------------------
+        // dispatch({type: 'book/getBooks/pending',payload: undefined})
         try {
             const response = await fetch("http://localhost:3005/books")
             const data = await response.json()
             return data
+            // dispatch({type: 'book/getBooks/fulfilled',payload: undefined})
+
         } catch (err) {
             console.log(err);
+            // dispatch({type: 'book/getBooks/rejected',payload: undefined})
+
         }
 
     },
 )
+// part1
+// ----------------------------------------------------------------
+//getBooks: {pendings,fulfilled,rejected}
+//getBooks -> createasyncThunk -> 3 types of actions
+//pending createAction('book/getBooks/pending',(payload) =>{return payload})
+//fulfilled createAction('book/getBooks/fulfilled',(payload) =>{return payload})
+//rejected createAction('book/getBooks/rejected',(payload) =>{return payload})
 
 const bookSlice = createSlice({
     name: "book",
